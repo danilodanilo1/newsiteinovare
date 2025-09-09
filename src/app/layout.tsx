35bd159a3cf5b2eb847cdec13239badb42a5da63
danilodanilo1 <danilo.onascimento@hotmail.com>
2025-09-09 18:58:24 -0300

    adding new googletagmanager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import StyledComponentsRegistry from "@/lib/registry";
 import 'swiper/css';
 import 'swiper/css/pagination'; // Estilo para os "pontinhos" de navegação
 import GlobalStyles from "@/styles/GlobalStyles";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WBRMKKR');`}}></Script>
+        {/* // <!-- End Google Tag Manager --> */}
+
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WBRMKKR"
+          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
+
+        {/* <!-- Global site tag (gtag.js) - Google Ads: 10876347005 --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10876347005"></script>
+        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-10876347005');
+        `}}>
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
